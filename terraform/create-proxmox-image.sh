0bfa986eec3b69ad11576cd2eef66c9778bd2c6e
Jared McQueen <jaredmcqueen@gmail.com>
2022-07-08 23:56:43 -0400

    refactored and updated terraform files

--- terraform/create-proxmox-image.sh
+++ terraform/create-proxmox-image.sh
@@ -1,13 +1,14 @@
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-qm create 9000 -name ubuntu-cloudinit -memory 1024 -net0 virtio,bridge=vmbr0 -cores 1 -sockets 1
+virt-customize -a jammy-server-cloudimg-amd64.img --install qemu-guest-agent
+qm destroy 9000
+qm create 9000 --name ubuntu-jammy --memory 1024 --net0 virtio,bridge=vmbr0 --cores 1 --sockets 1
 qm importdisk 9000 jammy-server-cloudimg-amd64.img local-lvm
-virt-customize -a focal-server-cloudimg-amd64.img --install qemu-guest-agent
-qm set 9000 -scsihw virtio-scsi-pci -virtio0 local-lvm:vm-9000-disk-0
-qm set 9000 -serial0 socket
-qm set 9000 -boot c -bootdisk virtio0
-qm set 9000 -agent 1
-qm set 9000 -hotplug disk,network,usb
-qm set 9000 -vcpus 1
-qm set 9000 -vga qxl
-qm set 9000 -ide2 local-lvm:cloudinit
+qm set 9000 --scsihw virtio-scsi-pci --virtio0 local-lvm:vm-9000-disk-0
+qm set 9000 --serial0 socket
+qm set 9000 --boot c --bootdisk virtio0
+qm set 9000 --agent enabled=1
+qm set 9000 --hotplug disk,network,usb
+qm set 9000 --vcpus 1
+qm set 9000 --vga qxl
+qm set 9000 --ide2 local-lvm:cloudinit
 qm template 9000
--- terraform/ubuntu-servers.tf
+++ terraform/ubuntu-servers.tf
@@ -1,61 +1,52 @@
-variable "vm_count" {
-  type    = number
-  default = 1
+variable "kubernetes_nodes" {
+  type = list(object({
+    hostname = string
+    ip       = string
+    gw       = string
+    vmid     = number
+  }))
+  default = [
+    {
+      hostname = "k8s-node-1"
+      ip       = "10.0.6.32"
+      gw       = "10.0.6.1"
+      vmid     = 101
+    }
+  ]
 }
 
-variable "project_prefix" {
-  type    = string
-  default = "dev-vm"
+variable "node_options" {
+  type = object({
+    image        = string
+    memory       = number
+    cores        = number
+    diskSize     = string
+    diskLocation = string
+    bridge       = string
+  })
+  default = {
+    image        = "ubuntu-jammy"
+    memory       = 4096
+    cores        = 4
+    diskSize     = "25G"
+    diskLocation = "local-lvm"
+    bridge       = "vmbr1"
+  }
 }
 
-# will result in 10.0.6.31, 32, 33, etc.
-variable "ip_address_prefix" {
-  type    = string
-  default = "10.0.6.3"
-}
-
-variable "ip_address_gateway" {
-  type    = string
-  default = "10.0.6.1"
-}
-
-variable "pve_username" {
-  type    = string
-  default = "root@pam"
-}
-
-variable "pve_password" {
-  type = string
-}
-
-variable "pve_ip" {
-  type    = string
-  default = "10.0.0.7"
-}
-
-variable "pve_hostname" {
-  type    = string
-  default = "apollo"
-}
-
-variable "memory" {
-  type    = number
-  default = 4096
-}
-
-variable "cores" {
-  type    = number
-  default = 4
-}
-
-variable "diskSize" {
-  type    = string
-  default = "15G"
-}
-
-variable "diskLocation" {
-  type    = string
-  default = "local-lvm"
+variable "pve_host" {
+  type = object({
+    username = string
+    password = string
+    ip       = string
+    hostname = string
+  })
+  default = {
+    username = "root@pam"
+    password = "proxmox"
+    ip       = "10.0.0.8"
+    hostname = "apollo"
+  }
 }
 
 terraform {
@@ -68,40 +59,38 @@ terraform {
 
 provider "proxmox" {
   pm_tls_insecure = true
-  pm_api_url      = "https://${var.pve_ip}:8006/api2/json"
-  pm_user         = var.pve_username
-  pm_password     = var.pve_password
+  pm_api_url      = "https://${var.pve_host.ip}:8006/api2/json"
+  pm_user         = var.pve_host.username
+  pm_password     = var.pve_host.password
 }
 
 resource "proxmox_vm_qemu" "proxmox_vm" {
-  count       = var.vm_count
-  name        = "${var.project_prefix}-${count.index + 1}"
+  count       = length(var.kubernetes_nodes)
+  name        = var.kubernetes_nodes[count.index].hostname
   agent       = 1
-  vmid        = "30${count.index}"
-  target_node = var.pve_hostname
-  clone       = "jammy-cloudinit"
+  vmid        = var.kubernetes_nodes[count.index].vmid
+  target_node = var.pve_host.hostname
+  clone       = var.node_options.image
   full_clone  = true
-  # os_type     = "l26"
   os_type     = "cloud-init"
-  cores       = var.cores
+  cores       = var.node_options.cores
   sockets     = "1"
   cpu         = "host"
-  memory      = var.memory
+  memory      = var.node_options.memory
   scsihw      = "virtio-scsi-pci"
   bootdisk    = "scsi0"
 
   disk {
-#    slot     = 0
-    type     = "virtio"
-    storage  = var.diskLocation
-    size     = var.diskSize
-    discard  = "on"
+    #    slot     = 0
+    type    = "virtio"
+    storage = var.node_options.diskLocation
+    size    = var.node_options.diskSize
+    discard = "on"
   }
 
-  # vmbr1 is my VM network in proxmox 
   network {
     model  = "virtio"
-    bridge = "vmbr6"
+    bridge = var.node_options.bridge
   }
 
   lifecycle {
@@ -111,6 +100,6 @@ resource "proxmox_vm_qemu" "proxmox_vm" {
   }
 
   # Cloud Init Settings
-  ipconfig0               = "ip=${var.ip_address_prefix}${count.index + 1}/24,gw=${var.ip_address_gateway}"
-  sshkeys                 = file("~/.ssh/id_rsa.pub")
+  ipconfig0 = "ip=${var.kubernetes_nodes[count.index].ip}/24,gw=${var.kubernetes_nodes[count.index].gw}"
+  sshkeys   = file("~/.ssh/id_rsa.pub")
 }
